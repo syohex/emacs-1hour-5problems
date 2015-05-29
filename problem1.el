8144164e5e83fb51a8dd620910bf892648ddf269
Syohei YOSHIDA <syohex@gmail.com>
2015-05-29 20:20:16 +0900

    add tail recursive version

--- problem1.el
+++ problem1.el
@@ -17,8 +17,16 @@
       0
     (+ (car lst) (problem1-recursive (cdr lst)))))
 
+(defun problem1-tail-recursive (lst)
+  (cl-labels ((func (lst acc)
+                (if (null lst)
+                    acc
+                  (func (cdr lst) (+ acc (car lst))))))
+    (func lst 0)))
+
 (let ((input '(1 2 3 4 5 6 7 8 9 10)))
   (cl-assert (= 55
                 (problem1-for input)
                 (problem1-while input)
-                (problem1-recursive input))))
+                (problem1-recursive input)
+                (problem1-tail-recursive input))))

f6aa487ea16a70e6ed243517dee4526a7fcc4d4f
Syohei YOSHIDA <syohex@gmail.com>
2015-06-01 14:34:05 +0900

    fix bug

--- problem4.el
+++ problem4.el
@@ -1,9 +1,14 @@
 (require 'cl-lib)
 
+(defun compare (a b)
+  (let ((ab-val (string-to-number (format "%s%s" a b)))
+        (ba-val (string-to-number (format "%s%s" b a))))
+    (> ab-val ba-val)))
+
 (defun problem4 (lst)
-  (let ((xs (sort lst (lambda (a b)
-                        (string< (number-to-string b) (number-to-string a))))))
+  (let ((xs (sort lst #'compare)))
     (string-to-number (mapconcat #'number-to-string xs ""))))
 
 (cl-assert
+ (= (problem4 '(420 42 423)) 42423420)
  (= (problem4 '(50 2 1 9)) 95021))
